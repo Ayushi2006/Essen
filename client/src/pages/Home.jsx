4c29aebd55f4d08f79b73b95835d716dbea69f98
Ayushi <73694291+Ayushi2006@users.noreply.github.com>
2026-05-25 17:28:27 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer"; 
 import { motion } from "framer-motion";
 
 function Home() { 
